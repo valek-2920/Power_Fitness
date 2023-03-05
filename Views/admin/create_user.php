6ae1dd0af8a72127d95166d5580e31a444c3f372
valek-2920 <valek2920@gmail.com>
2023-03-05 10:33:12 -0600

    Add new pages in Views

---
+++ Views/admin/create_user.php
@@ -0,0 +1,190 @@
+<?php
+include('../utilities.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Power Fitness | Usuarios</title>
+
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="../vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+    <link href="../vendors/select2/dist/css/select2.min.css" rel="stylesheet">
+    <link href="../vendors/switchery/dist/switchery.min.css" rel="stylesheet">
+    <link href="../vendors/starrr/dist/starrr.css" rel="stylesheet">
+    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="../css/custom.min.css" rel="stylesheet">
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <?php
+            navAdmin();
+            ?>
+            <div class="right_col" role="main">
+                <div class="">
+                    <div class="page-title">
+                        <div class="title_left">
+                            <h3>Crear Usuario</h3>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 ">
+                            <div class="x_panel">
+                                <div class="x_content">
+                                    <br />
+                                    <form data-parsley-validate class="form-horizontal form-label-left">
+
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Primer Nombre <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Primer Apellido <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Segundo Apellido <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Correo <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="email" required="required" class="form-control">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Contraseña <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input class="form-control" required="required" type="password">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Genero</label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <div id="gender" class="btn-group" data-toggle="buttons">
+                                                    <label class="btn btn-secondary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                                        <input type="radio" name="gender" value="Masculino" class="join-btn"> &nbsp; Masculino &nbsp;
+                                                    </label>
+                                                    <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                                                        <input type="radio" name="gender" value="Femenino" class="join-btn"> Femenino
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Fecha Nacimiento <span class="required">*</span>
+                                            </label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input id="birthday" class="date-picker form-control" placeholder="dd-mm-yyyy" type="text" required="required" type="text" onfocus="this.type='date'" onmouseover="this.type='date'" onclick="this.type='date'" onblur="this.type='text'" onmouseout="timeFunctionLong(this)">
+                                                <script>
+                                                    function timeFunctionLong(input) {
+                                                        setTimeout(function() {
+                                                            input.type = 'text';
+                                                        }, 60000);
+                                                    }
+                                                </script>
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Celular <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Provincia <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <select id="heard" class="form-control" required>
+                                                    <option value="">***Seleccionar Provincia***</option>
+                                                    <option value="San José">San José</option>
+                                                    <option value="Alajuela">Alajuela</option>
+                                                    <option value="Cartago">Cartago</option>
+                                                    <option value="Heredia">Heredia</option>
+                                                    <option value="Guanacaste">Guanacaste</option>
+                                                    <option value="Puntarenas">Puntarenas</option>
+                                                    <option value="Limón">Limón</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Ciudad <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Codigo Postal <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control ">
+                                            </div>
+                                        </div>
+
+                                        <div class="item form-group">
+                                            <label class="col-form-label col-md-3 col-sm-3 label-align">Dirección Exacta <span class="required">*</span></label>
+                                            <div class="col-md-6 col-sm-6 ">
+                                                <input type="text" required="required" class="form-control">
+                                            </div>
+                                        </div>
+
+                                        <div class="ln_solid"></div>
+                                        <div class="item form-group">
+                                            <div class="col-md-6 col-sm-6 offset-md-3">
+                                                <button class="btn btn-danger" type="button">Cancelar</button>
+                                                <button type="submit" class="btn btn-primary">Crear</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            footerAdmin();
+            ?>
+        </div>
+    </div>
+
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <script src="../vendors/iCheck/icheck.min.js"></script>
+    <script src="../vendors/moment/min/moment.min.js"></script>
+    <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <script src="../vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
+    <script src="../vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
+    <script src="../vendors/google-code-prettify/src/prettify.js"></script>
+    <script src="../vendors/jquery.tagsinput/src/jquery.tagsinput.js"></script>
+    <script src="../vendors/switchery/dist/switchery.min.js"></script>
+    <script src="../vendors/select2/dist/js/select2.full.min.js"></script>
+    <script src="../vendors/parsleyjs/dist/parsley.min.js"></script>
+    <script src="../vendors/autosize/dist/autosize.min.js"></script>
+    <script src="../vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js"></script>
+    <script src="../vendors/starrr/dist/starrr.js"></script>
+    <script src="../js/custom.min.js"></script>
+
+</body>
+
+</html>
--- Views/utilities.php
+++ Views/utilities.php
@@ -30,28 +30,47 @@ function navClient(){
 
 function navAdmin(){
   echo '
-  <div class="menu" id="menu">
-  <div class="container">
-     <div class="logo">
-        <a href="index.php"><img src="../images/logo.png" alt=""/></a>
-     </div>
-     <div class="h_menu4"><!-- start h_menu4 -->
-       <a class="toggleMenu" href="#">Menu</a>
-         <ul class="nav">
-           <li class="active"><a href="index.php">Home</a></li>
-           <li><a href="about.php">About</a></li>
-           <li><a href="trainers.php">Trainers</a></li>
-           <li><a href="classes.php">Classes</a>
-               
-           </li>
-           <li><a href="blog.php">Blog</a></li>
-           <li><a href="pricing.php">Pricing</a></li>
-           <li><a href="contact.php">Contact</a></li>
-         </ul>
-          <script type="text/javascript" src="../js/nav.js"></script>
-      </div><!-- end h_menu4 -->
-     <div class="clear"></div>
+  <div class="col-md-3 left_col">
+  <div class="left_col scroll-view">
+      <div class="navbar nav_title" style="border: 0;">
+          <a href="index.php" class="site_title"><span>Power Fitness Administration</span></a>
+      </div>
+
+      <div class="clearfix"></div>
+      <br />
+
+      <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+          <div class="menu_section">
+              <ul class="nav side-menu">
+                  <li><a href="create_user.php"><i class="fa fa-users"></i> Usuarios</a></li>
+              </ul>
+          </div>
+      </div>
   </div>
+</div>
+<div class="top_nav">
+  <div class="nav_menu">
+      <div class="nav toggle">
+          <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+      </div>
+      <nav class="nav navbar-nav">
+          <ul class=" navbar-right">
+              <li class="nav-item dropdown open" style="padding-left: 15px;">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+                      User name
+                  </a>
+                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="javascript:;"> Perfil</a>
+                      <a class="dropdown-item" href="javascript:;">
+                          <span>Recuperar Contraseña</span>
+                      </a>
+                      <a class="dropdown-item" href="login.html"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesión</a>
+                  </div>
+              </li>
+          </ul>
+      </nav>
+  </div>
+</div>
  </div>
   ';
  }
@@ -61,7 +80,7 @@ function navAdmin(){
   <div class="footer-bottom">
   <div class="container">
    <div class="row section group">
-   <div class="col-md-4">
+   <div class="col-md-6">
      <div class="f-logo">
        <img src="../images/logo.png" alt=""/>
      </div>
@@ -69,7 +88,7 @@ function navAdmin(){
      <p class="address">Phone : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="m_10">(00) 222 666 444</span></p>
      <p class="address">Email : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="m_10">info[at]mycompany.com</span></p>
    </div>
-   <div class="col-md-4">
+   <div class="col-md-6">
      <ul class="list">
        <h4 class="m_7">Menu</h4>
        <li><a href="#">About</a></li>
@@ -101,5 +120,14 @@ function navAdmin(){
   </div>
   ';
  }
- 
-?>
+
+ function footerAdmin(){
+  echo '
+  <footer>
+  <div class="pull-right">
+      PowerFitness - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+  ';
+ }
